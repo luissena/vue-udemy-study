df5182c282bc2e4edb201350b4a1d9d6093d3444
luissena <luis.senarode@gmail.com>
2022-10-06 12:29:36 -0300

    challenge 06

---
+++ cmp-communication-assignment-problem/src/App.vue
@@ -0,0 +1,67 @@
+<template>
+    <div id="main">
+        <active-user :username="user.name" :userage="user.age"/> <!-- username (props) => data (object user) // userage == username -->
+        <user-data @set-data="setUserData"/> <!-- set-data (emit) => setUserData (method) => setar novos valores p object user -->
+    </div>
+</template>
+
+<script>
+    export default {
+      data() {
+        return {
+          user: {
+            name: "Luis Sena",
+            age: 19,
+          },
+        };
+      },
+      methods: {
+        setUserData(name,age){
+            this.user = {
+                name: name,
+                age: age
+            }
+        }
+      }
+    };
+    </script>
+
+<style>
+
+    *{
+        margin: 0;
+        padding: 0;
+    }
+
+    #main{
+        -webkit-box-shadow: 0px 0px 9px -2px rgba(0,0,0,0.18);
+        -moz-box-shadow: 0px 0px 9px -2px rgba(0,0,0,0.18);
+        box-shadow: 0px 0px 9px -2px rgba(0,0,0,0.18);
+        width: 50%;
+        height: 200px;
+        margin: auto;
+    }
+
+    #active-user{
+        margin: auto;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    
+        width: 50%;
+
+        font-size: 2rem;
+
+        
+    }
+
+    form{
+        margin: auto;
+        width: 50%;
+        display: flex;
+        flex-direction: column;
+    }
+    input{
+        padding: 5px;
+    }
+</style>
---
+++ cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,27 @@
+<template>
+    <section>
+        <form @submit.prevent="submitData"> <!-- submit => chama o submitData(method) -->
+            <input type="text" placeholder="Your name" v-model="enteredName"/>
+            <input type="number" placeholder="Your age" v-model="enteredAge" />
+            <button>Set User Data</button>
+        </form>
+    </section>
+</template>
+
+<script>
+    export default{
+        emits: ['set-data'], // Criação do emit
+        data(){
+            return{
+                enteredName: "",
+                enteredAge: "",
+            };
+        },
+        methods: {
+            submitData(){
+                this.$emit('set-data',this.enteredName ,this.enteredAge) // envia o emit?
+            }
+        }
+    }
+</script>
+
